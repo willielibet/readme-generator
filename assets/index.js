3aef2fdd024baea3002cc3055be74c86afee57ba
Willie <write2willie@gmail.com>
2021-04-13 13:38:11 -0400

    commit #4: testing returned promise in index.js, april132021, 1.38pm

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,5 @@
 // fs is a Node standard library package for reading and writing files
-const fs = require('fs');
+// const fs = require('fs');
 
 // TODO: Include packages needed for this application
 
@@ -8,84 +8,85 @@ const fs = require('fs');
 
 const questions = require('inquirer');
 
-const generateMarkdown = require("./assets/utils/generateMarkdown");
+// const generateMarkdown = require("./assets/utils/generateMarkdown");
 
-questions
-.prompt([ 
-{
-    type: "input",
-    name: "title",
-    message: "What is the Project Title?"
-},
-{
-    type: "input",
-    name: "description",
-    message: "Describe the task: "
-},
-{
-    type: "input",
-    name: "content",
-    message: "Enter Table of Contents"
-},
-{
-    type: "input",
-    name: "installation",
-    message: "What is the directory structure"
-},
-{
-    type: "input",
-    name: "installation",
-    message: "What are the installation steps?"
-},
-
-{
-    type: "input",
-    name: "usage",
-    message: "Enter usage"
-},
-
-{
-    type: "input",
-    name: "license",
-    message: "What is the license?"
-},
-{
-    type: "input",
-    name: "contributions",
-    message: "Who contributed to this project?"
-},
-{
-    type: "input",
-    name: "test",
-    message: "How do you test this project?"
-},
-{
-    type: "input",
-    name: "questions",
-    message: "Do you have any questions?"
-},
-]
-.then((data) => {
-// response.confirm === response.password
-//   ? console.log('Success!')
-//   : console.log('You forgot your password already?!')
-});
+questions.prompt([ 
+    {
+        type: "input",
+        name: "title",
+        message: "What is the Project Title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe the task: "
+    },
+    // {
+    //     type: "input",
+    //     name: "content",
+    //     message: "Enter Table of Contents"
+    // },
+    // {
+    //     type: "input",
+    //     name: "installation",
+    //     message: "What is the directory structure?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "installation",
+    //     message: "What are the installation steps?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "usage",
+    //     message: "Enter usage"
+    // },
+    // {
+    //     type: "input",
+    //     name: "license",
+    //     message: "What is the license?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "contributions",
+    //     message: "Who contributed to this project?"
+    // },
+    // {
+    //     type: "input",
+    //     name: "test",
+    //     message: "How do you test this project?"
+    // },
+    {
+        type: "input",
+        name: "questions",
+        message: "Do you have any questions?"
+    }
+    ]).then((data) => {
+    // response.confirm === response.password
+    //   ? console.log('Success!')
+    //   : console.log('You forgot your password already?!')
+        console.log(data);
+    });
 
 
-function writeToFile(fileName, data) {
-   fs.writeFile(fileName, data, function (error){
-    console.log("Success!");
-   });
-}  
+// function createREAME(fileName, data) {
+
+//     fs.writeFile(fileName.toUpperCase().split(' ').join('').md, data, function (error){
+//         if(err) {
+//             console.log(err)
+//         }
+//         console.log("Success! The README file was created!");
+//     });
+// }  
 
 // TODO: Create a function to write README file
 //function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
 
 
 
