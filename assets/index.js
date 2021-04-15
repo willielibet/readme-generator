6daa09af5c9f3ccc3a6c24909739759299c7745b
Willie <write2willie@gmail.com>
2021-04-14 22:55:36 -0400

    comit #23: added author name to different licenses capitalizing first letter of each name, april142021, 10.55pm

--- assets/index.js
+++ assets/index.js
@@ -45,7 +45,7 @@ questions.prompt([
             "Apache License, Version 2.0",
             "BSD-3-Clause",
             "MIT License",
-            "Mozilla Public License 1.1 (MPL 1.1)"
+            "Mozilla Public License 2.0 (MPL-2.0)"
         ]
     },
     {
@@ -72,25 +72,47 @@ questions.prompt([
         type: "input",
         name: "email",
         message: "What is your email?"
+    },  
+    {
+        type: "input",
+        name: "copyrightYear",
+        message: "What is the copyright year?"
+    },  
+    {
+        type: "input",
+        name: "author",
+        message: "Who is the author?"
     }
-  
     ]).then((data) => {
         console.log(data);
 
+        //source: https://www.freecodecamp.org/news/how-to-capitalize-words-in-javascript/
+        let author = `${data.author}`;
+        //split author's first and last names into an array of words to
+        //manipulate each word individually. 
+        author = author.split(" ");
+
+        //iterate over each word
+        for(let i=0; i < author.length; i++) {
+            author[i] = author[i][0].toUpperCase() + author[i].substr(1);
+        }
+
+        //join the words
+        author = author.join(' '); //.replace(/,/g, ' ');
+    
+        // author = author[0].toUpperCase() + author.substring(1);
+        console.log("Author is: " + author);
+
+
         // fs.writeFile(fileName.toUpperCase().split(' ').join('').md, data, function (error){
             // fs.writeFile("README.md", gMarkdown(data.toString()), function (error){
-        fs.writeFile("README.md", generateMarkdown(data), function (err){
+        fs.writeFile(`(${data.title}).md`, generateMarkdown(data,author), function (err){
             if(err) {
                 console.log(err)
             }
             console.log("Success! The README file was created!");
         });
     });
-
-
-
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 // function init() {}
--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -19,40 +19,88 @@ function renderLicenseBadge(license) {
     license += " This application is covered under the MIT license.";
     return license;
   }
-  else if(license == "Mozilla Public License 1.1 (MPL 1.1)"){
-    license = "![APM](https://img.shields.io/apm/l/npm?style=plastic)<br />";
+  else if(license == "Mozilla Public License 2.0 (MPL-2.0)"){
+    license = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)<br />";
     license += " This application is covered under the Mozilla Public License 1.1 (MPL 1.1) license.";
     return license;
   }
   else {
     return "";
   }
-  
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license,copyrightYear,author) {
+  if(license == "Apache License, Version 2.0"){
+    license = `  Copyright [${copyrightYear}] [${author}]
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.`
+    return license;
+  }
+  else if(license == "BSD-3-Clause"){
+    license = `   Copyright [${copyrightYear}] [${author}]
+    
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+      LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+      ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+      (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+      SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`;
+    return license;
+  }
+  else if(license == "MIT License"){
+    license = `  Copyright [${copyrightYear}] [${author}]
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+    return license;
+  }
+  else if(license == "Mozilla Public License 2.0 (MPL-2.0)"){
+    license = `  Copyright [${copyrightYear}] [${author}]
+
+    Covered Software is provided under this License on an “as is” basis, 
+    without warranty of any kind, either expressed, implied, or statutory, including, 
+    without limitation, warranties that the Covered Software is free of defects, 
+    merchantable, fit for a particular purpose or non-infringing. The entire risk 
+    as to the quality and performance of the Covered Software is with You. Should 
+    any Covered Software prove defective in any respect, You (not any Contributor) 
+    assume the cost of any necessary servicing, repair, or correction. This disclaimer 
+    of warranty constitutes an essential part of this License. No use of any Covered 
+    Software is authorized under this License except under this disclaimer.`
+    return license;
+  }
+  else {
+    return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  if (renderLicenseBadge(data.license)) {
-    console.log("called badge function");
-    let badge = renderLicenseBadge(data.license);
-    console.log("function renderBadge: " + renderLicenseBadge(data.license));
-  }
+function generateMarkdown(data, author) {
   return `# License 
   ${renderLicenseBadge(data.license)}
 
-  # Title
 ##  ${data.title}
-  # Description
-  ${data.description}
-## ${data.tableOfContents} 
+
+## Table of Contents
   [Title](#title)  
   [Description](#description)  
   [Installation](#installation)  
@@ -60,16 +108,15 @@ function generateMarkdown(data) {
   [Licenses](#licenses)  
   [Contribution](#contribution)  
   [Test](#test)  
-  [GitHub Username](#username)  
-  [Email](#email)
+  [Questions](#questions)
 
   ## Installation
   ${data.installation}
   ## Usage
   ${data.usage}
 
-  ## Licenses
-  ${data.license}
+  ## License
+  ${renderLicenseLink(data.license, data.copyrightYear,author)}
 
   ## Contribution
   ${data.contributions} 
@@ -77,12 +124,9 @@ function generateMarkdown(data) {
   ## Test
   ${data.test}
 
-  ## GitHub User
-  ${data.username}  
-  [GitHub](https://github.com/${data.username})   
-
-  ## Email
-  ${data.email}
+  ## Questions 
+  [${data.username}'s GitHub](https://github.com/${data.username})   
+  Reach me with additional questions at ${data.email}.
 `;
 }
 
